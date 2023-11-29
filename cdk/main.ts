963ac8680bbbae1428d161d36921ed500b1c2b2c
Your Name <you@example.com>
2023-11-29 13:30:14 +0700

    feat: add example docker

--- cdk/main.ts
+++ cdk/main.ts
@@ -1,14 +1,36 @@
 import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
+import { DockerProvider } from "@cdktf/provider-docker/lib/provider";
+import { Image } from "@cdktf/provider-docker/lib/image";
+import { Container } from "@cdktf/provider-docker/lib/container";
 
 class MyStack extends TerraformStack {
-  constructor(scope: Construct, id: string) {
-    super(scope, id);
+  constructor(scope: Construct, name: string) {
+    super(scope, name);
 
-    // define resources here
+    new DockerProvider(this, "docker", {});
+
+    const dockerImage = new Image(this, "nginxImage", {
+      name: "nginx:latest",
+      keepLocally: false,
+    });
+
+    console.log(dockerImage)
+
+    new Container(this, "nginxContainer", {
+      name: "tutorial",
+      image: dockerImage.name,
+      // image: dockerImage.imageId, <--- :D
+      ports: [
+        {
+          internal: 80,
+          external: 8000,
+        },
+      ],
+    });
   }
 }
 
 const app = new App();
-new MyStack(app, "test");
+new MyStack(app, "learn-cdktf-docker");
 app.synth();
